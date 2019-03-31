a960c335ccbf9341977bdad931777865f750c050
santa <ryancurtis998@yahoo.com>
2019-03-31 20:08:55 +0300

    addition of profile.servicee

---
+++ src/app/profile.service.spec.ts
@@ -0,0 +1,7 @@
+import { Profile.Service } from './profile.service';
+
+describe('Profile.Service', () => {
+  it('should create an instance', () => {
+    expect(new Profile.Service()).toBeTruthy();
+  });
+});
