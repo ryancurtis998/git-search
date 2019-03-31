d70e7989b8cfdf8b617779bd7297c7629afbf9a4
santa <ryancurtis998@yahoo.com>
2019-03-31 20:04:27 +0300

    addition of comment.pipe

---
+++ src/app/comment.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { Comment.Pipe } from './comment.pipe';
+
+describe('Comment.Pipe', () => {
+  it('should create an instance', () => {
+    expect(new Comment.Pipe()).toBeTruthy();
+  });
+});
