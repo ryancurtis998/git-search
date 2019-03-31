0ec39d6dcc6ef948c29307ecbff6f58d1badfb79
santa <ryancurtis998@yahoo.com>
2019-03-31 20:45:02 +0300

    addition of ts file

--- src/app/profile.service.spec.ts
+++ src/app/profile.service.spec.ts
@@ -1,4 +1,15 @@
-import { Profile.Service } from './profile.service';
+import { TestBed } from '@angular/core/testing';
+
+import { ProfileService } from './profile.service';
+
+describe('ProfileService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ProfileService = TestBed.get(ProfileService);
+    expect(service).toBeTruthy();
+  });
+});import { Profile.Service } from './profile.service';
 
 describe('Profile.Service', () => {
   it('should create an instance', () => {
