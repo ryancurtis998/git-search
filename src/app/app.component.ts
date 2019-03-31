d980ca9ca112eb92c59c92c296c19187ecc4b927
santa <ryancurtis998@yahoo.com>
2019-03-31 20:00:38 +0300

    addition of color.directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {HttpClient} from  '@angular/common/http';
+import {ProfileService} from './profile.service';
+// import {SearchComponent} from './profile/profile.component';
+
+
+import {Repository} from './repository';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers : [ProfileService]
 })
-export class AppComponent {
-  title = 'git-search';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { GitSearchComponent } from './git-search/git-search.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SearchComponent } from './search/search.component';
+import { CommentPipe } from './comment.pipe';
+import { ColorDirective } from './color.directive';
 
+
+const routes: Routes = [
+  { path: "profile", component: ProfileComponent },
+  { path: "search", component: SearchComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    GitSearchComponent,
     ProfileComponent,
-    SearchComponent
+    SearchComponent,
+    CommentPipe,
+    ColorDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgProgressModule,
+    NgProgressHttpModule ,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
